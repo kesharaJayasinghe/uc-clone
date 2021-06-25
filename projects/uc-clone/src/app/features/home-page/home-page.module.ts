542f2043b427c5b112a96dbd48bd85d5d9c78650
Keshara Jayasinghe <kesharalahiru1@gmail.com>
2021-06-25 23:49:48 +0530

    carousel list + styling

--- projects/uc-clone/src/app/features/home-page/home-page.module.ts
+++ projects/uc-clone/src/app/features/home-page/home-page.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { CarouselListComponent } from './components/carousel-list/carousel-list.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CarouselListComponent
   ],
   imports: [
     CommonModule,
     HomePageRoutingModule,
-    SharedModule
+    SharedModule,
+    CarouselModule
   ]
 })
 export class HomePageModule { }
